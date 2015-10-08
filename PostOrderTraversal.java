7d22a39d52eb54257a6ebd721fb71147ad41ec0c
Illuminardy <yanardy.sanchez@gmail.com>
2015-10-07 20:08:50 -0400

    Delete node from singly-linked list

---
+++ PostOrderTraversal.java
@@ -0,0 +1,29 @@
+/***** Problem Statement *****/
+// Given a binary tree, return the postorder traversal of its nodes' values.
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class PostOrderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> results = new ArrayList<Integer>();
+        return postorderTraversal(root, results);
+    }
+    
+    public List<Integer> postorderTraversal(TreeNode node, List<Integer> list){
+        
+        if (node != null){
+            list = postorderTraversal(node.left, list);
+            list = postorderTraversal(node.right, list);
+            list.add(node.val);
+        }
+        
+        return list;
+    }
+}
