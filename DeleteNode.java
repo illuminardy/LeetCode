6c61dc54d4703e804aafcac9ed5146ef64b8e311
Illuminardy <yanardy.sanchez@gmail.com>
2015-10-07 20:14:40 -0400

    Delete node from singly-linked list

--- DeleteNode.java
+++ DeleteNode.java
@@ -1,3 +1,6 @@
+/***** Problem Statement *****/
+// Write a function to delete a node (except the tail) in a singly linked list,  given only access to that node.
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
